a5f456de3fd81946e2a18a8450c47a6c4f2d1a0d
Gurgenci <h.gurgenci@uq.edu.au>
2021-05-21 14:41:54 +1000

    README.md now had the link to documentation

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Test and document creation of a github project
 Operating system : Windows 10 Enterprise 2016
 
 I document the time through print statements in the file "helloworld.py"
+
+You can find the final documentation here: https://helloworlddocumentation.readthedocs.io/en/latest/ 
--- helloworld.py
+++ helloworld.py
@@ -6,9 +6,16 @@ Test and document creation of a github project
 
 @author: e4hgurge
 """
-import os, time,datetime,sys
+import datetime
 stf="%d%b%y"
 sep=" --- "
+
+logfilename="gitlog.txt"
+logactions=False
+printactions=True
+
+if logactions:
+    logfile=open(logfilename,"w")
 
 def getday(y,m,d):
     """
@@ -22,48 +29,68 @@ def getday(y,m,d):
     """
     return datetime.datetime(y,m,d).strftime(stf)+sep 
 
+def la(s):
+    if printactions:
+        print(s)
+    if logactions:
+        logfile.write(s+"\n")
+        
+
 #
 t=getday(2021,5,20)
-print(t+"New Folder: H:\My Documents\git\helloworld")
-print(t+"'git init': Initialise empty git repository")
-print(t+"Create README.md using a text editor")
-print(t+"'git add README.md' : Stage the README file")
-print(t+"Create a file PRIVATE.txt as a file to be ignored")
-print(t+"Create '.gitignore' using a text editor with 'PRIVATE.txt' on its first line")
-print(t+"'git status' to confirm that 'git' ignores PRIVATE.txt")
-print(t+"'git add .gitignore' : Stage the file '.gitignore'")
-print(t+"'git commit -am \"First Commit\"")
-print(t+"Login to github and create a new empty repository on 'github.com' named 'helloworld'.")
-print(t+"\ngit remote add origin https://github.com/Gurgenci/helloworld.git (needs local git credential to have been set before)\
+la(t+"New Folder: H:\My Documents\git\helloworld")
+la(t+"'git init': Initialise empty git repository")
+la(t+"Create README.md using a text editor")
+la(t+"'git add README.md' : Stage the README file")
+la(t+"Create a file PRIVATE.txt as a file to be ignored")
+la(t+"Create '.gitignore' using a text editor with 'PRIVATE.txt' on its first line")
+la(t+"'git status' to confirm that 'git' ignores PRIVATE.txt")
+la(t+"'git add .gitignore' : Stage the file '.gitignore'")
+la(t+"'git commit -am \"First Commit\"")
+la(t+"Login to github and create a new empty repository on 'github.com' named 'helloworld'.")
+la(t+"\ngit remote add origin https://github.com/Gurgenci/helloworld.git (needs local git credential to have been set before)\
 \ngit branch -M main\ngit push -u origin main")
-print(t+"Start VS Code on 'helloworld' folder and start a terminal (windows powershell)")
-print(t+"'mkdir docs'   The docs folder will contain all documentation.")
-print(t+"'cd docs'  --> we are now in 'H:\My Documents\git\helloworld\docs'")
-print(t+"pip install sphinx (unless you already have done so)")
-print(t+"'sphinx-quickstart' and then answer questions:\n\
+la(t+"Start VS Code on 'helloworld' folder and start a terminal (windows powershell)")
+la(t+"'mkdir docs'   The docs folder will contain all documentation.")
+la(t+"'cd docs'  --> we are now in 'H:\My Documents\git\helloworld\docs'")
+la(t+"pip install sphinx (unless you already have done so)")
+la(t+"'sphinx-quickstart' and then answer questions:\n\
 > Separate source and build directories (y/n) [n] : y\n\
 > Project name: helloworld\n> Author name(s) : Hal Gurgenci\n\
 > Project release []: May 2021\n\
 > Project language [en]: \n\
 This creates an initial directory structure")
-print(t+"Edit 'conf.py' in 'docs\source' folder as follows:")
-print("\textensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest','sphinx.ext.mathjax',\
+la(t+"Edit 'conf.py' in 'docs\source' folder as follows:")
+la("\textensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest','sphinx.ext.mathjax',\
 'sphinx.ext.viewcode']")
-print(t+"'./make html' : To create a scaffold to hold all documentation")
-print(t+"Edit 'index.rst' and set the title to 'Helloworld as a tutorial for github documentation'")
-print(t+"Create the LICENSE.rst file in 'docs' folder with my one-line title and MIT License text")
-print(t+"Ornamental changes to index.rst")
+la(t+"'./make html' : To create a scaffold to hold all documentation")
+la(t+"Edit 'index.rst' and set the title to 'Helloworld as a tutorial for github documentation'")
+la(t+"Create the LICENSE.rst file in 'docs' folder with my one-line title and MIT License text")
+la(t+"Ornamental changes to index.rst")
 #
 t=datetime.datetime(2021,5,21).strftime(stf)+sep 
-print(t+"Create 'api.tst' in 'docs' folder.  It includes:\n\
+la(t+"Create 'api.tst' in 'docs' folder.  It includes:\n\
 helloworld API reference\n\
 ========================\n\
 \n\
 .. automodule:: helloworld\n\
     :members:\n\
 ")
-print(t+"Add 'api.rst' to the toctree in 'index.rst'.  'a' should align with the letter 't' of 'toctree'")
-print(t+"Add the 'helloworld.py' path to 'conf.py' so that sphinx can find it:\n\
+la(t+"Add 'api.rst' to the toctree in 'index.rst'.  'a' should align with the letter 't' of 'toctree'")
+la(t+"Add the 'helloworld.py' path to 'conf.py' so that sphinx can find it:\n\
 \tsys.path.insert(0, os.path.abspath('..\\..'))")
-print(t+"'git add docs'")
-print(t+"'git commit -am 'adding the first commit with documentation'")
+la(t+"'git add docs'")
+la(t+"'git commit -am 'adding the first commit with documentation'")
+la(t+"'git push origin main")
+la(t+"Go to 'https://readthedocs.org/' and register if you do not have an account")
+la(t+"Import a project and pick 'helloworld' from github to import\n\
+\tReadTheDocs did not accept helloworld as a suitable project name so I entered 'HelloWorldDocumentation'\n\
+\tas the name of the project in 'ReadTheDocs'.  It does not have to be same name as in 'github'.")
+la(t+"Build the document using the latest github version")
+la(t+"This creates the web page 'https://helloworlddocumentation.readthedocs.io/en/latest/'")
+la("I add this address to README.md and this concludes the documentation")
+#
+#
+if logactions:
+    logfile.close()
+
